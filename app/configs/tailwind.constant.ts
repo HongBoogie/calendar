41212dd3fe5fed315a9733f91ab2a814ffb30f96
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-01-08 22:17:18 +0900

    chore: 프로젝트 초기 설정
    - tanstack-query, date-fns 설치
    - 상수 설정, tailwind-config 설정
    - tsconfig에 절대경로 설정

---
+++ app/configs/tailwind.constant.ts
@@ -0,0 +1,4 @@
+export const LAYOUT_MIN_WIDTH = '58.125rem';
+export const HEADER_HEIGHT = '5rem'; 
+
+export const HOME_MIN_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,11 @@
 import type { Config } from "tailwindcss";
+import {
+  // layout
+  LAYOUT_MIN_WIDTH,
+  HEADER_HEIGHT,
+  // home
+  HOME_MIN_HEIGHT,
+} from "./app/configs/tailwind.constant"
 
 const config: Config = {
   content: [
@@ -8,9 +15,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        header: HEADER_HEIGHT,
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      minHeight: {
+        home: HOME_MIN_HEIGHT,
+      },
+      minWidth: {
+        layout: LAYOUT_MIN_WIDTH,
+      },
+      padding: {
+        header: HEADER_HEIGHT,
+        
       },
     },
   },
