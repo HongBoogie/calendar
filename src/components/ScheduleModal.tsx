2112b83f4bc89fa6fee09f1105c430b08fe1c70f
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-01-11 03:59:34 +0900

    feat: 상세 정보 모달
    - CalendarWrapper에서 props로 받아온 dateObj, schedule을 바탕으로 정보를 뿌려줌
    - deleteSchedule도 props로 받아오면 zustand 의존성을 줄일 수 있나 싶음.
    - 삭제 후 리렌더링이 안되는 문제가 있어서, 상위 컴포넌트에서 리렌더링 촉발하도록 수정

--- src/components/ScheduleModal.tsx
+++ src/components/ScheduleModal.tsx
@@ -1,22 +1,55 @@
 'use client';
 
-import { type ButtonHTMLAttributes, type PropsWithChildren, type ComponentPropsWithoutRef } from 'react';
-import clsx from 'clsx';
+import { type ComponentPropsWithoutRef } from 'react';
+import { DateObj, Schedule } from '@/libs/internalTypes';
 import Modal from './Modal';
+import { useScheduleStore } from '@/store/ScheduleStore';
 
-type Props = ComponentPropsWithoutRef<typeof Modal> & {};
+type Props = ComponentPropsWithoutRef<typeof Modal> & {
+  DateObj: DateObj;
+  schedule: Schedule | null;
+};
 
-const ScheduleModal = ({children, close} : Props) => {
+const ScheduleModal = ({ close, DateObj, schedule } : Props) => {
+
+  const deleteSchedule = useScheduleStore(state => state.deleteSchedule);
+
+  const handleDelete = () => {
+    if (schedule) {
+      deleteSchedule(schedule.id);
+      close();
+    }
+  }
+
   return (
     <Modal close={close}>
-      <h2 id="modal-title" className="mb-2 text-center">
-        일정 추가
-      </h2>
-      <p id="modal-description" className=" mb-4 text-center">
-        {children}
-      </p>
+      {
+        schedule && (
+          <button
+        onClick={handleDelete}
+        className='flex w-12 justify-center ml-auto bg-sky-300 rounded-lg text-white'
+      >
+        삭제
+      </button>
+        )
+      }
+      <div className="text-center">
+        {schedule ? (
+          <>
+            <h3 className="text-lg font-bold mb-2">{schedule.title}</h3>
+            <p className="mb-2">{schedule.description}</p>
+            {schedule.startTime && (
+              <p>
+                {schedule.startTime} ~ {schedule.endTime}
+              </p>
+            )}
+          </>
+        ) : (
+          <p>일정이 없습니다.</p>
+        )}
+      </div>
     </Modal>
   )
 }
 
-export default ScheduleModal
+export default ScheduleModal;
