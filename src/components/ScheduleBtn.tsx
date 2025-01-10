fc8ef0386c0b49a2c51af61e05f1a8c404351c84
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-01-11 03:52:59 +0900

    feat: 스케줄 버튼 구현
    - 굳이.. 이렇게 할 필요가 있었나 싶긴함.
    - Headless 형태로 해보자 했는데 큰 의미는 없는듯 함. 추후 수정할듯

---
+++ src/components/ScheduleBtn.tsx
@@ -0,0 +1,21 @@
+import React, { type ReactNode } from 'react'
+
+type ScheduleBtnProps = {
+  children: React.ReactNode;
+  className?: string;
+}
+
+type Props = ScheduleBtnProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const ScheduleBtn = ({children, className, ...props} : Props) => {
+  return (
+    <button
+      className={className}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default ScheduleBtn;
