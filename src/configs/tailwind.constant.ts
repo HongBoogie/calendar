ac99c92d115fb884ac70e208d0987305f1900c1a
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-01-20 23:45:07 +0900

    fix: 매직 넘버 수정
    - CALENDAR_LENGTH 를 35 -> 42로 변경함으로써 달력에 통일성 부여

--- src/configs/tailwind.constant.ts
+++ src/configs/tailwind.constant.ts
@@ -6,7 +6,7 @@ export const HOME_MIN_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
 export const CALENDAR_MIN_HEIGHT = `calc(100vh - ${CALENDAR_HEIGHT})`;
 
 export const DATE_MONTH_FIXER = 1;
-export const CALENDER_LENGTH = 35;
+export const CALENDAR_LENGTH = 42;
 export const DEFAULT_TRASH_VALUE = 0;
 export const DAY_OF_WEEK = 7;
 export const DAY_LIST = ['일', '월', '화', '수', '목', '금', '토'];
